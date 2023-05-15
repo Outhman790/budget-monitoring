b07e43606d93ac1e21480d367e0fd8d9a6279200
Outhman790 <39206094+Outhman790@users.noreply.github.com>
2023-05-15 11:36:07 +0100

    Update circleChart.js

--- src/components/circleChart.js
+++ src/components/circleChart.js
@@ -1,58 +1,62 @@
-import React, { useContext, useState, useEffect } from "react";
+import React from "react";
+import { Chart as ChartJS } from "chart.js/auto";
 import { GlobalContext } from "../context/GlobalState";
-import ChartC from "./ChartC";
+import { useContext } from "react";
+import { Pie } from "react-chartjs-2";
 
-function CircleChart() {
-  const { transactions } = useContext(GlobalContext);
+function selectionSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
 
-  const [chartData, setChartData] = useState({
-    labels: transactions
-      .filter((transaction) => transaction.type === "Expense")
-      .map((transaction) => transaction.category),
-    datasets: [
-      {
-        label: "Category",
-        data: transactions
-          .filter((transaction) => transaction.type === "Expense")
-          .map((transaction) => transaction.amount),
-        backgroundColor: [
-          "#d9ed92",
-          "#99d98c",
-          "#34a0a4",
-          "168aad",
-          "1e6091",
-          "184e77",
-        ],
-      },
-    ],
-  });
-
-  useEffect(() => {
-    setChartData((prevChartData) => ({
-      ...prevChartData,
-      labels: transactions
-        .filter((transaction) => transaction.type === "Expense")
-        .map((transaction) => transaction.category),
-      datasets: [
-        {
-          ...prevChartData.datasets[0],
-          data: transactions
-            .filter((transaction) => transaction.type === "Expense")
-            .map((transaction) => transaction.amount),
-          backgroundColor: [
-            "#d9ed92",
-            "#99d98c",
-            "#34a0a4",
-            "168aad",
-            "1e6091",
-            "184e77",
-          ],
-        },
-      ],
-    }));
-  }, [transactions]);
-
-  return <ChartC data={chartData} />;
+  return arr;
 }
 
-export default CircleChart;
+export default function ChartC({ data }) {
+  const { transactions } = useContext(GlobalContext);
+  const expenses = transactions
+    .filter((transaction) => transaction.type === "Expense")
+    .map((transaction) => transaction.amount);
+  const test = selectionSort(expenses);
+  const categories = transactions
+    .filter(
+      (transaction) =>
+        transaction.amount === test[test.length - 1] ||
+        transaction.amount === test[test.length - 2]
+    )
+    .map((transaction) => transaction.category);
+  console.log(categories);
+
+  return (
+    <div className="canva">
+      <Pie data={data} />
+      {/* if the expenses exits */}
+      {transactions.length > 0 ? (
+        <div className="rectangle">
+          {/* if there's more than one expense */}
+          {transactions.length > 1 ? (
+            <h4 className="h4">
+              You should try to save in
+              <span className="danger"> {categories[0]}</span>
+              {transactions.length <= 2}
+            </h4>
+          ) : (
+            // if there's just one expense
+            <h4 className="h4">Please add more categories </h4>
+          )}
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+}
